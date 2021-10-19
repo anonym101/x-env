4a4a72c747048981b3c673e5f3c43a3151649d85
anonym101 <anon1@mailfence.com>
2021-10-19 14:02:52 +0000

    minor

---
+++ src/XEnv/XEnvV2/index.ts
@@ -0,0 +1,9 @@
+
+
+// todo
+/**
+ * use cross-env with dotenv and extend-dotenv
+ * use read-env-file to load as object
+ * - simplify code, as we wont provide it to the masses for now
+ * - use more functional programming over oop, not need to be sp descriptive
+ */
