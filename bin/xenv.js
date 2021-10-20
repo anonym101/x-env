e7acbbc8f2563c9a03fccac7cfa638a613b3c8e4
anonym101 <anon1@mailfence.com>
2021-10-20 07:44:13 +0000

    set index and bin

--- bin/xenv.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-
-(function (){
-    const xEnvConfig = require('../index')
-    const crossEnv = require('cross-env/src/')
-    crossEnv(process.argv.slice(2))   
-    xEnvConfig()
-})()
---
+++ xenv.js
@@ -0,0 +1,71 @@
+
+/**
+ * @typedef { 'TEST' | 'DEVELOPMENT' | 'PRODUCTION'} ENVIRONMENT
+ * @typedef {{[prop: string]: string}} ENV
+ * @typedef {{[name:string]:Array<string> }} NAME_CONVENTIONS
+ */
+
+ const dotEnvConfig  = require('dotenv').config
+ const variableExpansion =require('dotenv-expand')
+ 
+
+ /** @type {NAME_CONVENTIONS} */
+const ENV_NAME_CONVENTIONS = {
+    DEVELOPMENT: ['DEV', 'dev', 'develop', 'DEVELOP', 'DEVELOPMENT', 'development'],
+    PRODUCTION: ['PROD', 'prod', 'PRODUCTION', 'production'],
+    TEST: ['TEST', 'test', 'TESTING', 'testing'],
+}
+
+
+/**
+ *
+ * Match to current NODE_ENV/ENVIRONMENT by comparison 
+ * @param {NODE_ENV} string
+ * @returns {ENVIRONMENT}
+ **/
+const matchEnv = (NODE_ENV) => {
+    if (!NODE_ENV) return undefined
+    return Object.entries(ENV_NAME_CONVENTIONS).reduce((n, [k, val]) => {
+        if (!n) {
+            /** @type {Array<string>} */
+            let _val = val
+            if (_val.filter((x) => x === NODE_ENV).length) n = k
+        }
+        return n
+    }, '')
+}
+
+
+ /**
+  * Return parsed ENVIRONMENT {name.env} based on process.env.NODE_ENV
+  * - NODE_ENV available values matching ENV_NAME_CONVENTIONS[]
+  * @param {boolean} auto decide which file to load based on process.env.NODE_ENV
+  * @param {string} pth
+  * 
+  * @returns {ENV}
+  */
+module.exports = xEnvConfig = (auto = true, pth) => {
+     if (!auto && !pth) throw 'When auto not set must provide path'
+ 
+     /** @type {ENVIRONMENT} */
+     const NODE_ENV= process.env.NODE_ENV
+ 
+     let envPath = pth
+     if (auto) {
+         if (!NODE_ENV) throw 'NODE_ENV NOT SET'
+         if (matchEnv(NODE_ENV) === 'DEVELOPMENT') envPath = `./dev.env`
+         if (matchEnv(NODE_ENV) === 'PRODUCTION') envPath = `./prod.env`
+         if (matchEnv(NODE_ENV) === 'TEST') envPath = `./test.env`
+     } 
+     
+     try {
+         const parsed = dotEnvConfig({ path: envPath })
+         const d = variableExpansion(parsed)
+         if (d.error) throw d.error
+         else return d.parsed
+     } catch (err) {
+         console.log('[parsedEnvConfig]', `ENVIRONMENT not found for: ${envPath || NODE_ENV}`)
+     }
+     return undefined
+ }
+ 
