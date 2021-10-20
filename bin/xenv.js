b389d28a7b341afeef119e3d8520488b4a3a8ffe
anonym101 <anon1@mailfence.com>
2021-10-20 08:01:09 +0000

    update readme

--- bin/xenv.js
+++ bin/xenv.js
@@ -1,10 +1,8 @@
 #!/usr/bin/env node
 
 (function (){
-    const xEnvConfig = require('./xenv')
+    const xEnvConfig = require('../xenv')
     const crossEnv = require('cross-env/src/')
     crossEnv(process.argv.slice(2))   
     xEnvConfig()
 })()
-
-console.log(process.env.NODE_ENV)
